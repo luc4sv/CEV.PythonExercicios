6767466efc5b2fec2d8023f4d7464c81439d5382
Lucas Vinícius <74675742+luc4sv@users.noreply.github.com>
2024-07-23 11:12:27 -0300

    Delete Aulas 0-4 directory

--- Aulas 0-4/desafio2.py
+++
@@ -1,5 +0,0 @@
-dia = input ('Que dia você nasceu?')
-mes = input ('Que mês você nasceu?')
-ano = input ('Que ano você nasceu?')
-print ('Você nasceu dia', dia, 'do mês', mes, 'e do ano de', ano)
-             
--- Aulas 0-4/desafio3.py
+++
@@ -1,3 +0,0 @@
-numero = input ('digite um número')
-numero2 = input ('digite outro numero')
-print ('A soma é', numero + numero2)
