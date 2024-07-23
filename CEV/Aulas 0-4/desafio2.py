340e0ac37c63eecba1be62214015218fdc369908
Lucas Vinícius <74675742+luc4sv@users.noreply.github.com>
2024-07-23 11:13:03 -0300

    Add files via upload

---
+++ CEV/Aulas 0-4/desafio2.py
@@ -0,0 +1,5 @@
+dia = input ('Que dia você nasceu?')
+mes = input ('Que mês você nasceu?')
+ano = input ('Que ano você nasceu?')
+print ('Você nasceu dia', dia, 'do mês', mes, 'e do ano de', ano)
+             
---
+++ CEV/Aulas 0-4/desafio3.py
@@ -0,0 +1,3 @@
+numero = input ('digite um número')
+numero2 = input ('digite outro numero')
+print ('A soma é', numero + numero2)
